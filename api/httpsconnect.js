fabbdf89bb92afb11e23d7804945d1758856ccac
21paradox <1036339815@qq.com>
2020-03-13 15:26:22 +0800

    init

---
+++ api/httpsconnect.js
@@ -0,0 +1,64 @@
+
+const serializeError = require('serialize-error');
+const net = require('net');
+const nkn = require('nkn-sdk');
+
+const useMultiClient = true;
+let Client = useMultiClient ? nkn.MultiClient : nkn.Client;
+
+module.exports = async (req, res) => {
+    let conncfgRaw = Buffer.from(req.headers.conncfg, 'base64');
+    let conncfg = JSON.parse(conncfgRaw);
+    let uid = req.headers.uid;
+    const reqAddr = req.headers.addr;
+    if (!conncfg) {
+        res.status(500).send('should provide reqcfg')
+        return;
+    }
+
+    const carrierClient = new Client({
+        identifier: uid,
+        seed: process.env.SEED,
+        responseTimeout: 8000,
+        encrypt: false
+    });
+    const nknClientReady = new Promise((resolve) => {
+        carrierClient.onConnect(resolve);
+    });
+
+    let target = net.connect(conncfg);
+    let connected = false;
+    let err = null;
+
+    target.on('connect', function () {
+        connected = true;
+        client.send(addr, '');
+    });
+
+    carrierClient.onMessage(async ({ src, payload }) => {
+        target.write(Buffer.from(payload));
+    });
+
+    target.on('data', async (data) => {
+        await nknClientReady;
+        carrierClient.send(reqAddr, data)
+    });
+
+    target.on('close', function () {
+        if (err) {
+            let errstr = JSON.stringify(serializeError(err), null, 4);
+            res.status(500).send(errstr);
+        } else {
+            res.status(200).send('');
+        }
+        carrierClient.close();
+    });
+
+    target.on('error', function (_err) {
+        err = _err;
+
+        if (!connected) {
+            res.status(500).send('err not connected');
+        }
+    });
+}
